b56817bae89f53b68d08e8a437bdd5fc78595f72
Sitiugin <sitiugin@my.yorku.ca>
2019-10-19 15:53:45 -0400

    - format event types in history recycler view

--- app/src/main/java/com/glebworx/pomodoro/ui/fragment/report/view/item/ReportHistoryItem.java
+++ app/src/main/java/com/glebworx/pomodoro/ui/fragment/report/view/item/ReportHistoryItem.java
@@ -1,6 +1,7 @@
 package com.glebworx.pomodoro.ui.fragment.report.view.item;
 
 import android.content.Context;
+import android.text.Html;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,13 @@ import com.mikepenz.fastadapter.FastAdapter;
 import com.mikepenz.fastadapter.items.AbstractItem;
 
 import java.util.List;
+
+import static com.glebworx.pomodoro.model.HistoryModel.EVENT_PROJECT_CREATED;
+import static com.glebworx.pomodoro.model.HistoryModel.EVENT_PROJECT_DELETED;
+import static com.glebworx.pomodoro.model.HistoryModel.EVENT_PROJECT_UPDATED;
+import static com.glebworx.pomodoro.model.HistoryModel.EVENT_TASK_CREATED;
+import static com.glebworx.pomodoro.model.HistoryModel.EVENT_TASK_DELETED;
+import static com.glebworx.pomodoro.model.HistoryModel.EVENT_TASK_UPDATED;
 
 public class ReportHistoryItem extends AbstractItem<ReportHistoryItem, ReportHistoryItem.ViewHolder> {
 
@@ -26,6 +34,7 @@ public class ReportHistoryItem extends AbstractItem<ReportHistoryItem, ReportHis
     public ReportHistoryItem(@NonNull HistoryModel model) {
         this.model = model;
     }
+
 
     //                                                                                    OVERRIDDEN
 
@@ -53,6 +62,40 @@ public class ReportHistoryItem extends AbstractItem<ReportHistoryItem, ReportHis
         return model.getId();
     }
 
+    private String getEventTypeString(Context context) {
+        switch (model.getEventType()) {
+            case EVENT_PROJECT_CREATED:
+                return context.getString(
+                        R.string.report_history_title_project_added,
+                        model.getName());
+            case EVENT_PROJECT_UPDATED:
+                return context.getString(
+                        R.string.report_history_title_project_updated,
+                        model.getName());
+            case EVENT_PROJECT_DELETED:
+                return context.getString(
+                        R.string.report_history_title_project_deleted,
+                        model.getName());
+            case EVENT_TASK_CREATED:
+                return context.getString(
+                        R.string.report_history_title_task_added,
+                        model.getTaskName(),
+                        model.getName());
+            case EVENT_TASK_UPDATED:
+                return context.getString(
+                        R.string.report_history_title_task_updated,
+                        model.getTaskName(),
+                        model.getName());
+            case EVENT_TASK_DELETED:
+                return context.getString(
+                        R.string.report_history_title_task_deleted,
+                        model.getTaskName(),
+                        model.getName());
+            default:
+                return null;
+        }
+    }
+
 
     //                                                                                   VIEW HOLDER
 
@@ -62,7 +105,7 @@ public class ReportHistoryItem extends AbstractItem<ReportHistoryItem, ReportHis
         private AppCompatTextView dateTextView;
         private AppCompatTextView eventTypeTextView;
 
-        public ViewHolder(View view) {
+        ViewHolder(View view) {
             super(view);
             this.context = view.getContext();
             this.dateTextView = view.findViewById(R.id.text_view_date);
@@ -72,7 +115,7 @@ public class ReportHistoryItem extends AbstractItem<ReportHistoryItem, ReportHis
         @Override
         public void bindView(@NonNull ReportHistoryItem item, @NonNull List<Object> payloads) {
             dateTextView.setText(item.getModel().getTimestamp().toString());
-            eventTypeTextView.setText(item.getModel().getEventType());
+            eventTypeTextView.setText(Html.fromHtml(item.getEventTypeString(context), 0));
         }
 
         @Override
