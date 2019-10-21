ce18ed6695902377da59d5bc914b05033297299b
Sitiugin <sitiugin@my.yorku.ca>
2019-10-21 07:14:00 -0400

    - add a class for chart initialization

--- app/src/main/java/com/glebworx/pomodoro/model/ReportPomodoroModel.java
+++ app/src/main/java/com/glebworx/pomodoro/model/ReportPomodoroModel.java
@@ -1,21 +1,38 @@
 package com.glebworx.pomodoro.model;
 
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.LineData;
+
 public class ReportPomodoroModel {
 
-    private int pomodorosCompleted;
+    private final int pomodorosCompleted;
+    private final LineData distributionData;
+    private final BarData trendsData;
 
     public ReportPomodoroModel() {
+        this.pomodorosCompleted = 0;
+        this.distributionData = new LineData();
+        this.trendsData = new BarData();
     }
 
-    public ReportPomodoroModel(int pomodorosCompleted) {
+    public ReportPomodoroModel(int pomodorosCompleted,
+                               LineData distributionData,
+                               BarData trendsData) {
         this.pomodorosCompleted = pomodorosCompleted;
+        this.distributionData = distributionData;
+        this.trendsData = trendsData;
     }
 
     public int getPomodorosCompleted() {
         return pomodorosCompleted;
     }
 
-    public void setPomodorosCompleted(int pomodorosCompleted) {
-        this.pomodorosCompleted = pomodorosCompleted;
+    public LineData getDistributionData() {
+        return distributionData;
     }
+
+    public BarData getTrendsData() {
+        return trendsData;
+    }
+
 }
--- app/src/main/java/com/glebworx/pomodoro/ui/fragment/report/view/ReportPomodorosViewPresenter.java
+++ app/src/main/java/com/glebworx/pomodoro/ui/fragment/report/view/ReportPomodorosViewPresenter.java
@@ -2,12 +2,17 @@ package com.glebworx.pomodoro.ui.fragment.report.view;
 
 import androidx.annotation.NonNull;
 
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.LineData;
 import com.glebworx.pomodoro.api.HistoryApi;
 import com.glebworx.pomodoro.model.ReportPomodoroModel;
 import com.glebworx.pomodoro.ui.fragment.report.view.interfaces.IReportPomodorosView;
 import com.glebworx.pomodoro.ui.fragment.report.view.interfaces.IReportPomodorosViewPresenter;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -39,10 +44,22 @@ public class ReportPomodorosViewPresenter implements IReportPomodorosViewPresent
 
     private Observable<ReportPomodoroModel> getObservable(QuerySnapshot snapshot) {
         return Observable.create(emitter -> {
-            ReportPomodoroModel model = new ReportPomodoroModel(snapshot.getDocuments().size());
+            List<DocumentSnapshot> documentSnapshots = snapshot.getDocuments();
+            ReportPomodoroModel model = new ReportPomodoroModel(
+                    documentSnapshots.size(),
+                    getDistributionData(documentSnapshots),
+                    getTrendsData(documentSnapshots));
             emitter.onNext(model);
             emitter.onComplete();
         });
+    }
+
+    private LineData getDistributionData(List<DocumentSnapshot> documentSnapshots) {
+        return new LineData();
+    }
+
+    private BarData getTrendsData(List<DocumentSnapshot> documentSnapshots) {
+        return new BarData();
     }
 
 }
