5a84abeb84c4f820d72a3ce7deafb24b16a5c6b3
Sitiugin <sitiugin@my.yorku.ca>
2019-11-07 04:59:49 -0500

    - now able to receive broadcasts

--- app/src/main/java/com/glebworx/pomodoro/util/NotificationReceiver.java
+++ app/src/main/java/com/glebworx/pomodoro/util/NotificationReceiver.java
@@ -3,18 +3,31 @@ package com.glebworx.pomodoro.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class NotificationReceiver extends BroadcastReceiver {
 
     public static final String KEY_ACTION = "action";
 
-    public static final String ACTION_START = "action_start";
-    public static final String ACTION_PAUSE = "action_pause";
-    public static final String ACTION_RESUME = "action_resume";
+    public static final String ACTION_START = "ACTION_START_TASK";
+    public static final String ACTION_PAUSE = "ACTION_PAUSE_TASK";
+    public static final String ACTION_RESUME = "ACTION_RESUME_TASK";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context, "ACTION CLICKED", Toast.LENGTH_LONG).show();
+        String action = intent.getStringExtra(KEY_ACTION);
+        if (action == null) {
+            return;
+        }
+        switch (action) {
+            case ACTION_START:
+                break;
+            case ACTION_PAUSE:
+                break;
+            case ACTION_RESUME:
+                break;
+        }
     }
 
 }
