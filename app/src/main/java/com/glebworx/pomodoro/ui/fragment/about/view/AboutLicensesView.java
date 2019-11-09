baff31b1fe4aaf094bb6f5b3200b0dc847346fe6
Sitiugin <sitiugin@my.yorku.ca>
2019-11-09 16:03:18 -0500

    - implement about item

--- app/src/main/java/com/glebworx/pomodoro/ui/fragment/about/view/AboutLicensesView.java
+++ app/src/main/java/com/glebworx/pomodoro/ui/fragment/about/view/AboutLicensesView.java
@@ -1,8 +1,10 @@
 package com.glebworx.pomodoro.ui.fragment.about.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.Html;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
@@ -10,8 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.glebworx.pomodoro.R;
 import com.glebworx.pomodoro.ui.fragment.about.view.interfaces.IAboutLicensesView;
 import com.glebworx.pomodoro.ui.fragment.about.view.item.AboutLicenseItem;
+import com.glebworx.pomodoro.util.manager.DialogManager;
 import com.mikepenz.fastadapter.FastAdapter;
 import com.mikepenz.fastadapter.adapters.ItemAdapter;
 import com.mikepenz.itemanimators.AlphaCrossFadeAnimator;
@@ -58,14 +62,25 @@ public class AboutLicensesView extends RecyclerView implements IAboutLicensesVie
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        //RecyclerView rootView = (RecyclerView) inflate(context, R.layout.view_about_licenses, this);
         this.context = context;
         presenter = new AboutLicensesViewPresenter(this, context);
     }
 
     private void showUriDialog(String uri) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        context.startActivity(browserIntent);
+
+        if (context instanceof Activity) {
+            DialogManager.showGenericDialog(
+                    (Activity) context,
+                    R.id.container_main,
+                    R.string.about_licenses_title_open_in_browser,
+                    Html.fromHtml(context.getString(R.string.about_licenses_text_open_in_browser, uri), 0),
+                    R.string.core_open,
+                    () -> {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                        context.startActivity(browserIntent);
+                    });
+        }
+
     }
 
 }
--- app/src/main/java/com/glebworx/pomodoro/util/manager/DialogManager.java
+++ app/src/main/java/com/glebworx/pomodoro/util/manager/DialogManager.java
@@ -1,6 +1,7 @@
 package com.glebworx.pomodoro.util.manager;
 
 import android.app.Activity;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -47,23 +48,59 @@ public class DialogManager {
 
         positiveButton.setText(positiveButtonTextId);
 
-        View.OnClickListener onClickListener = view -> {
-            if (!alertDialog.isShowing()) {
-                return;
-            }
-            if (view.getId() == R.id.button_positive) {
-                alertDialog.dismiss();
-                callback.onPositiveButtonClicked();
-            } else if (view.getId() == R.id.button_negative) {
-                alertDialog.dismiss();
-            }
-        };
+        View.OnClickListener onClickListener = getOnClickListener(alertDialog, callback);
 
         dialogView.findViewById(R.id.button_negative).setOnClickListener(onClickListener);
         positiveButton.setOnClickListener(onClickListener);
 
         alertDialog.show();
 
+    }
+
+    public static void showGenericDialog(@NonNull Activity activity,
+                                         int rootViewId,
+                                         int titleId,
+                                         Spanned description,
+                                         int positiveButtonTextId,
+                                         IDialogManager callback) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_generic, activity.findViewById(rootViewId), false);
+        dialogBuilder.setView(dialogView);
+        AlertDialog alertDialog = dialogBuilder.create();
+
+        ((AppCompatTextView) dialogView.findViewById(R.id.text_view_title)).setText(titleId);
+        ((AppCompatTextView) dialogView.findViewById(R.id.text_view_description)).setText(description);
+
+        AppCompatButton positiveButton = dialogView.findViewById(R.id.button_positive);
+
+        positiveButton.setText(positiveButtonTextId);
+
+        View.OnClickListener onClickListener = getOnClickListener(alertDialog, callback);
+
+        dialogView.findViewById(R.id.button_negative).setOnClickListener(onClickListener);
+        positiveButton.setOnClickListener(onClickListener);
+
+        alertDialog.show();
+
+    }
+
+    private static View.OnClickListener getOnClickListener(AlertDialog alertDialog, IDialogManager callback) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!alertDialog.isShowing()) {
+                    return;
+                }
+                if (view.getId() == R.id.button_positive) {
+                    alertDialog.dismiss();
+                    callback.onPositiveButtonClicked();
+                } else if (view.getId() == R.id.button_negative) {
+                    alertDialog.dismiss();
+                }
+            }
+        };
     }
 
     public interface IDialogManager {
