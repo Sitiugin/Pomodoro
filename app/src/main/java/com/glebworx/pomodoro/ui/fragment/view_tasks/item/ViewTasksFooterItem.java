890d6737e0f6a00d8f489cf9d98f5567d5195cdd
Sitiugin <sitiugin@my.yorku.ca>
2019-11-19 05:04:07 -0500

    - implement view tasks footer

--- app/src/main/java/com/glebworx/pomodoro/ui/fragment/view_tasks/item/ViewTasksFooterItem.java
+++ app/src/main/java/com/glebworx/pomodoro/ui/fragment/view_tasks/item/ViewTasksFooterItem.java
@@ -1,4 +1,51 @@
 package com.glebworx.pomodoro.ui.fragment.view_tasks.item;
 
-public class ViewTasksFooterItem {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.glebworx.pomodoro.R;
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.items.AbstractItem;
+
+import java.util.List;
+
+public class ViewTasksFooterItem extends AbstractItem<ViewTasksFooterItem, ViewTasksFooterItem.ViewHolder> {
+
+    public ViewTasksFooterItem() {
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder getViewHolder(@NonNull View view) {
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getType() {
+        return R.id.item_view_tasks_footer;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.item_view_tasks_footer;
+    }
+
+    //                                                                                   VIEW HOLDER
+
+    protected static class ViewHolder extends FastAdapter.ViewHolder<ViewTasksFooterItem> {
+
+        ViewHolder(View view) {
+            super(view);
+        }
+
+        @Override
+        public void bindView(@NonNull ViewTasksFooterItem item, @NonNull List<Object> payloads) {
+        }
+
+        @Override
+        public void unbindView(@NonNull ViewTasksFooterItem item) {
+        }
+
+    }
 }
